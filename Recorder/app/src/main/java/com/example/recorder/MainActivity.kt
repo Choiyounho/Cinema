1ae3721814abfb1449f9560936a0229c797c2cc4
Choiyounho <maxosa78@gmail.com>
2021-04-14 20:48:28 +0900

    [feat] : 녹음 기능 구현

--- Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
+++ Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.recorder
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.recorder.State.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val resetButton: Button by lazy {
+        findViewById(R.id.resetButton)
+    }
 
     private val recordingButton: RecordingButton by lazy {
         findViewById(R.id.recordButton)
@@ -14,7 +22,19 @@ class MainActivity : AppCompatActivity() {
 
     private val requiredPermission = arrayOf(Manifest.permission.RECORD_AUDIO)
 
-    private var state = State.BEFORE_RECORDING
+    private val recordingFilePath: String by lazy {
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+
+    private var state = BEFORE_RECORDING
+    set(value) {
+        field = value
+        resetButton.isEnabled = (value == AFTER_RECODING || value == ON_PLAYING)
+        recordingButton.updateIconWithState(value)
+    }
+
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +42,7 @@ class MainActivity : AppCompatActivity() {
 
         requestAudioPermission()
         initViews()
+        bindViews()
     }
 
     override fun onRequestPermissionsResult(
@@ -39,7 +60,6 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "권한이 없어 종료됩니다.", Toast.LENGTH_SHORT).show()
             finish()
         }
-
     }
 
     private fun requestAudioPermission() {
@@ -48,6 +68,62 @@ class MainActivity : AppCompatActivity() {
 
     private fun initViews() {
         recordingButton.updateIconWithState(state)
+    }
+
+    private fun bindViews() {
+        resetButton.setOnClickListener {
+            stopPlaying()
+            state = BEFORE_RECORDING
+        }
+
+        recordingButton.setOnClickListener {
+            when(state) {
+                BEFORE_RECORDING -> startRecording()
+                ON_RECORDING -> stopRecording()
+                AFTER_RECODING -> startPlaying()
+                ON_PLAYING -> stopPlaying()
+            }
+        }
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(recordingFilePath)
+            prepare()
+        }
+        recorder?.start()
+        state = ON_RECORDING
+    }
+
+    private fun stopRecording() {
+        recorder?.run {
+            stop()
+            release()
+        }
+        recorder = null
+        state = AFTER_RECODING
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer().apply {
+            setDataSource(recordingFilePath)
+            prepare()
+        }
+        player?.start()
+        state = ON_PLAYING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+        state = AFTER_RECODING
+    }
+
+    private fun initVariable() {
+        state = BEFORE_RECORDING
     }
 
     companion object {

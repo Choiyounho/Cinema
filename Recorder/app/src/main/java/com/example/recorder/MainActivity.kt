7ae4e8ffe276e0dcf6c2054ebeb8b37609c49da7
Choiyounho <maxosa78@gmail.com>
2021-04-14 20:11:47 +0900

    [feat] : 녹음 권한 설정

--- Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
+++ Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.recorder
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,16 +12,45 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.recordButton)
     }
 
+    private val requiredPermission = arrayOf(Manifest.permission.RECORD_AUDIO)
+
     private var state = State.BEFORE_RECORDING
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestAudioPermission()
         initViews()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                    grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if (!audioRecordPermissionGranted) {
+            Toast.makeText(this, "권한이 없어 종료됩니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermission, REQUEST_RECORD_AUDIO_PERMISSION)
     }
 
     private fun initViews() {
         recordingButton.updateIconWithState(state)
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 }

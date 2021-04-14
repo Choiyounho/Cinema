b67e52cb9658c6f40ba87134279c57918ea685dc
Choiyounho <maxosa78@gmail.com>
2021-04-14 19:33:17 +0900

    [feat] : 녹음기 Ui 구성

---
+++ Recorder/app/src/main/java/com/example/recorder/RecordingButton.kt
@@ -0,0 +1,22 @@
+package com.example.recorder
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageButton
+import com.example.recorder.State.*
+
+class RecordingButton(
+    context: Context,
+    attrs: AttributeSet
+): AppCompatImageButton(context, attrs) {
+
+    fun updateIconWithState(state: State) {
+        when (state) {
+            BEFORE_RECORDING -> setImageResource(R.drawable.ic_record)
+            AFTER_RECODING -> setImageResource(R.drawable.ic_play)
+            ON_RECORDING -> setImageResource(R.drawable.ic_stop)
+            ON_PLAYING -> setImageResource(R.drawable.ic_stop)
+        }
+    }
+
+}

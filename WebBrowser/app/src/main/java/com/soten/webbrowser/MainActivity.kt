01d148ab4201c486ad366453795fda56392f6886
Choiyounho <maxosa78@gmail.com>
2021-04-15 15:56:56 +0900

    [feat] : Url 로딩 기능 구현

--- WebBrowser/app/src/main/java/com/soten/webbrowser/MainActivity.kt
+++ WebBrowser/app/src/main/java/com/soten/webbrowser/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.soten.webbrowser
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
+    private val addressBar: EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        bindViews()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
+        webView.apply {
+            webViewClient = WebViewClient()
+            webView.settings.javaScriptEnabled = true
+            webView.loadUrl("http://www.google.com")
+        }
+    }
+
+    private fun bindViews() {
+        addressBar.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                webView.loadUrl(v.text.toString())
+            }
+
+            return@setOnEditorActionListener false
+        }
     }
 }

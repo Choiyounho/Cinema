f8031fba39218bf14686952483e4b15dad64c7c9
Choiyounho <maxosa78@gmail.com>
2021-04-15 16:04:36 +0900

    [feat] : 네비게이션 기능 구현

--- WebBrowser/app/src/main/java/com/soten/webbrowser/MainActivity.kt
+++ WebBrowser/app/src/main/java/com/soten/webbrowser/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +19,18 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.addressBar)
     }
 
+    private val goHomeButton: ImageButton by lazy {
+        findViewById(R.id.homeButton)
+    }
+
+    private val goBackButton: ImageButton by lazy {
+        findViewById(R.id.backButton)
+    }
+
+    private val goForwardButton: ImageButton by lazy {
+        findViewById(R.id.forwardButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,22 +39,46 @@ class MainActivity : AppCompatActivity() {
         bindViews()
     }
 
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
     @SuppressLint("SetJavaScriptEnabled")
     private fun initViews() {
         webView.apply {
             webViewClient = WebViewClient()
             webView.settings.javaScriptEnabled = true
-            webView.loadUrl("http://www.google.com")
+            webView.loadUrl(DEFAULT_URL)
         }
     }
 
     private fun bindViews() {
-        addressBar.setOnEditorActionListener { v, actionId, event ->
+        goHomeButton.setOnClickListener {
+            webView.loadUrl(DEFAULT_URL)
+        }
+
+        addressBar.setOnEditorActionListener { v, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 webView.loadUrl(v.text.toString())
             }
 
             return@setOnEditorActionListener false
         }
+
+        goBackButton.setOnClickListener {
+            webView.goBack()
+        }
+
+        goForwardButton.setOnClickListener {
+            webView.goForward()
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_URL = "http://www.google.com"
     }
 }

fe9006bbdbce77a3107816b7486692f5a8117139
Choiyounho <maxosa78@gmail.com>
2021-05-03 20:56:30 +0900

    [feat] : Notification 구현 2

--- PushNotifier/app/src/main/java/com/soten/pushnotifier/MainActivity.kt
+++ PushNotifier/app/src/main/java/com/soten/pushnotifier/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.soten.pushnotifier
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.messaging.FirebaseMessaging
@@ -17,6 +19,14 @@ class MainActivity : AppCompatActivity() {
 
         // 파이어베이스에서 메세지가 오는 지 확인
         initFirebase()
+        updateResult()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        updateResult(true)
     }
 
     private fun initFirebase() {
@@ -26,5 +36,16 @@ class MainActivity : AppCompatActivity() {
                     binding.firebaseTokenTextView.text = task.result
                 }
             }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateResult(isNewIntent: Boolean = false) {
+        binding.resultTextView.text =
+            (intent.getStringExtra("notificationType") ?: "앱 런처 ") +
+                    if (isNewIntent) {
+                        "으로 갱신했습니다."
+                    } else {
+                        "실행 했습니다."
+                    }
     }
 }
--- PushNotifier/app/src/main/java/com/soten/pushnotifier/MyFirebaseMessagingService.kt
+++ PushNotifier/app/src/main/java/com/soten/pushnotifier/MyFirebaseMessagingService.kt
@@ -3,7 +3,10 @@ package com.soten.pushnotifier
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
@@ -58,11 +61,21 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         title: String?,
         message: String?
     ): Notification {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra("notificationType", "${type.title} 타입")
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+
+        // 타입 별로 인텐트를 생성
+        val pendingIntent = PendingIntent.getActivity(this, type.id, intent, FLAG_UPDATE_CURRENT)
+
         val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_baseline_notifications_24)
             .setContentTitle(title)
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         when (type) {
             NotificationType.NORMAL -> Unit

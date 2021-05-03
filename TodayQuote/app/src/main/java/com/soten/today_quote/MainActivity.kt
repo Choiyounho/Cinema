205e95f12bec8b46df32269a3581b8bb0d768be5
Choiyounho <maxosa78@gmail.com>
2021-05-04 02:02:29 +0900

    [feat] : Remote Config 구성, 연동

--- TodayQuote/app/src/main/java/com/soten/today_quote/MainActivity.kt
+++ TodayQuote/app/src/main/java/com/soten/today_quote/MainActivity.kt
@@ -3,6 +3,11 @@ package com.soten.today_quote
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +20,52 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initViews()
+        initData()
     }
 
     private fun initViews() {
-        viewPager.adapter = QuotesPagerAdapter(emptyList())
+    }
+
+    private fun initData() {
+        val remoteConfig = Firebase.remoteConfig
+        remoteConfig.setConfigSettingsAsync( // 서버에서 블락하기 전까지는 앱 실행할 때마다 업데이트
+            remoteConfigSettings {
+                minimumFetchIntervalInSeconds = 0
+            }
+        )
+
+        // fetch는 비동기로 서버와 통신하는 것이기 때문에 리스너가 필요
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            if (it.isSuccessful) {
+                val quotes = pareQuotesJson(remoteConfig.getString("quotes"))
+                val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
+
+                displayQuotesPager(quotes, isNameRevealed)
+
+
+            }
+        }
+    }
+
+    private fun pareQuotesJson(json: String): List<Quote> {
+        val jsonArray = JSONArray(json)
+        var jsonList = emptyList<JSONObject>()
+        for (index in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(index)
+            jsonObject?.let {
+                jsonList = jsonList + it
+            }
+        }
+
+        return jsonList.map {
+            Quote(quote = it.getString("quote"), name = it.getString("name"))
+        }
+    }
+
+    private fun displayQuotesPager(quotes: List<Quote>, isNameRevealed: Boolean) {
+        viewPager.adapter = QuotesPagerAdapter(
+            quotes = quotes,
+            isNameRevealed = isNameRevealed
+        )
     }
 }

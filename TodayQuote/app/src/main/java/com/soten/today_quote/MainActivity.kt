e47ea1a19785f789c8e903b9fc6fdd1fe6082644
Choiyounho <maxosa78@gmail.com>
2021-05-04 02:26:59 +0900

    [feat] : 무한 페이징

--- TodayQuote/app/src/main/java/com/soten/today_quote/MainActivity.kt
+++ TodayQuote/app/src/main/java/com/soten/today_quote/MainActivity.kt
@@ -2,17 +2,24 @@ package com.soten.today_quote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.viewpager2.widget.ViewPager2
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import org.json.JSONArray
 import org.json.JSONObject
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
 
     private val viewPager: ViewPager2 by lazy {
         findViewById(R.id.viewPager)
+    }
+
+    private val progressBar: ProgressBar by lazy {
+        findViewById(R.id.progressBar)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +31,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+        viewPager.setPageTransformer { page, position ->
+            when {
+                position.absoluteValue >= 1f -> {
+                    page.alpha = 0f
+                }
+                position == 0f -> {
+                    page.alpha = 1f
+                }
+                else -> {
+                    page.alpha = 1f - 2 * position.absoluteValue
+                }
+            }
+        }
     }
 
     private fun initData() {
@@ -36,13 +56,12 @@ class MainActivity : AppCompatActivity() {
 
         // fetch는 비동기로 서버와 통신하는 것이기 때문에 리스너가 필요
         remoteConfig.fetchAndActivate().addOnCompleteListener {
+            progressBar.visibility = View.GONE
             if (it.isSuccessful) {
                 val quotes = pareQuotesJson(remoteConfig.getString("quotes"))
                 val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
 
                 displayQuotesPager(quotes, isNameRevealed)
-
-
             }
         }
     }
@@ -63,9 +82,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun displayQuotesPager(quotes: List<Quote>, isNameRevealed: Boolean) {
-        viewPager.adapter = QuotesPagerAdapter(
+        val adapter = QuotesPagerAdapter(
             quotes = quotes,
             isNameRevealed = isNameRevealed
         )
+        viewPager.adapter = adapter
+        viewPager.setCurrentItem(adapter.itemCount / 2 - 2, false)
     }
 }

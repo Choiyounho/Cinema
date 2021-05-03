39f518e2d76a126378e0c8b7f1acde73024bfec2
Choiyounho <maxosa78@gmail.com>
2021-05-03 11:31:53 +0900

    [feat] : Notification 구현

--- PushNotifier/app/src/main/java/com/soten/pushnotifier/MainActivity.kt
+++ PushNotifier/app/src/main/java/com/soten/pushnotifier/MainActivity.kt
@@ -2,6 +2,7 @@ package com.soten.pushnotifier
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.messaging.FirebaseMessaging
 import com.soten.pushnotifier.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,16 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        // 파이어베이스에서 메세지가 오는 지 확인
+        initFirebase()
+    }
 
-
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    binding.firebaseTokenTextView.text = task.result
+                }
+            }
     }
 }

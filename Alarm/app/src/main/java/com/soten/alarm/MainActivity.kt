8ea3c071f2318d427fadb081d2efc0ada37412f0
Choiyounho <maxosa78@gmail.com>
2021-05-06 21:32:51 +0900

    [docs] : 알람 앱 viewBinding 추가

--- Alarm/app/src/main/java/com/soten/alarm/MainActivity.kt
+++ Alarm/app/src/main/java/com/soten/alarm/MainActivity.kt
@@ -2,10 +2,30 @@ package com.soten.alarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.soten.alarm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initOnOffButton()
+        initChangeAlramTimeButton()
+        // todo step 1 : 뷰 초기화
+        // todo step 2 : 데이터 가져오기
+        // todo step 2 : 뷰에 데이터 그려주기기
+    }
+
+    private fun initOnOffButton() {
+        TODO("Not yet implemented")
+    }
+
+    private fun initChangeAlramTimeButton() {
+        TODO("Not yet implemented")
     }
 }

0ecad89639dd991c78ac22b2a9e38c9c15c3e8c4
Choiyounho <maxosa78@gmail.com>
2021-05-11 02:29:35 +0900

    [refactor] : 도서리뷰앱, ReviewDao

--- BookReview/app/src/main/java/com/soten/book_review/dao/ReviewDao.kt
+++ BookReview/app/src/main/java/com/soten/book_review/dao/ReviewDao.kt
@@ -1,4 +1,17 @@
 package com.soten.book_review.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.soten.book_review.model.review.Review
+
+@Dao
 interface ReviewDao {
+
+    @Query("SELECT * FROM review WHERE uid = :uid")
+    fun getOne(uid: Int): Review
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun saveReview(review: Review)
 }

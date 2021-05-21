bffd3af26ecfb4bb4cb1120de1dcba35102d95ae
Choiyounho <maxosa78@gmail.com>
2021-05-21 15:35:17 +0900

    [feat] : (중고거래) 상품 목록 페이지 UI 구성

--- UsedTransaction/app/src/main/java/com/soten/usedtransaction/ui/home/HomeFragment.kt
+++ UsedTransaction/app/src/main/java/com/soten/usedtransaction/ui/home/HomeFragment.kt
@@ -1,7 +1,31 @@
 package com.soten.usedtransaction.ui.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.soten.usedtransaction.R
+import com.soten.usedtransaction.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    private var binding: FragmentHomeBinding? = null
+    private lateinit var articleAdapter: ArticleAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+
+        articleAdapter = ArticleAdapter()
+        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
+            add(ArticleModel("0", "aaaa", 10000000, "5000원", ""))
+            add(ArticleModel("0", "bbbb", 20000000, "7000원", ""))
+        })
+
+        fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+    }
+
 }

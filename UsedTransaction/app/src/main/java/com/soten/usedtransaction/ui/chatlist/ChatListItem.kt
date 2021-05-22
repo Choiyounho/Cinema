95a76ffb821657f0520be908237ebad8794f9540
Choiyounho <maxosa78@gmail.com>
2021-05-22 16:02:26 +0900

    [feat] : (중고거래) 채팅방 리스트 페이지 구현

---
+++ UsedTransaction/app/src/main/java/com/soten/usedtransaction/ui/chatlist/ChatListItem.kt
@@ -0,0 +1,12 @@
+package com.soten.usedtransaction.ui.chatlist
+
+data class ChatListItem(
+    val buyerId: String,
+    val sellerId: String,
+    val itemTitle: String,
+    val key: Long
+) {
+
+    constructor(): this("", "", "", 0)
+
+}
--- UsedTransaction/app/src/main/java/com/soten/usedtransaction/ui/home/HomeFragment.kt
+++ UsedTransaction/app/src/main/java/com/soten/usedtransaction/ui/home/HomeFragment.kt
@@ -11,14 +11,18 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.soten.usedtransaction.DBKey.Companion.CHILD_CHAT
 import com.soten.usedtransaction.DBKey.Companion.DB_ARTICLES
+import com.soten.usedtransaction.DBKey.Companion.DB_USERS
 import com.soten.usedtransaction.R
 import com.soten.usedtransaction.databinding.FragmentHomeBinding
+import com.soten.usedtransaction.ui.chatlist.ChatListItem
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var articleAdapter: ArticleAdapter
     private lateinit var articleDb: DatabaseReference
+    private lateinit var userDb: DatabaseReference
 
     private var binding: FragmentHomeBinding? = null
 
@@ -54,7 +58,41 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
         articleList.clear()
         articleDb = Firebase.database.reference.child(DB_ARTICLES)
-        articleAdapter = ArticleAdapter()
+        userDb = Firebase.database.reference.child(DB_USERS)
+        articleAdapter = ArticleAdapter(onItemClicked = { articleModel ->
+            if (auth.currentUser != null) {
+                // 로그인 상태
+                if (auth.currentUser?.uid != articleModel.sellerId) {
+                    val chatRoom = ChatListItem(
+                        buyerId = auth.currentUser?.uid.orEmpty(),
+                        sellerId = articleModel.sellerId,
+                        itemTitle = articleModel.title,
+                        key = System.currentTimeMillis()
+                    )
+
+                    userDb.child(auth.currentUser!!.uid)
+                        .child(CHILD_CHAT)
+                        .push()
+                        .setValue(chatRoom)
+
+                    userDb.child(articleModel.sellerId)
+                        .child(CHILD_CHAT)
+                        .push()
+                        .setValue(chatRoom)
+
+                    Snackbar.make(view, "채팅방이 생성되었습니다. 채팅탭에서 확인해주세요", Snackbar.LENGTH_SHORT).show()
+
+
+                } else {
+                    // 내가 올린 아이템
+                    Snackbar.make(view, "내가 올린 아이템입니다.", Snackbar.LENGTH_SHORT).show()
+                }
+            } else {
+                // 로그인 안 한 상태
+                Snackbar.make(view, "로그인 후 사용해주세요", Snackbar.LENGTH_SHORT).show()
+            }
+
+        })
 
         fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
         fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter

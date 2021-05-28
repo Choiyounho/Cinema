ad08033bebf409d436553760e7eb604d09dd0489
Choiyounho <maxosa78@gmail.com>
2021-05-29 02:29:50 +0900

    [feat] : (유튜브) MotionLayout과 RecyclerView 사이의 스크롤 기능 구현

--- YouTube/app/src/main/java/com/soten/youtube/ui/PlayerFragment.kt
+++ YouTube/app/src/main/java/com/soten/youtube/ui/PlayerFragment.kt
@@ -1,15 +1,28 @@
 package com.soten.youtube.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.soten.youtube.R
 import com.soten.youtube.databinding.FragmentPlayerBinding
+import com.soten.youtube.dto.VideoDto
+import com.soten.youtube.network.VideoService
+import com.soten.youtube.ui.adapter.VideoAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.math.abs
 
-class PlayerFragment: Fragment(R.layout.fragment_player) {
+class PlayerFragment : Fragment(R.layout.fragment_player) {
 
+    private lateinit var videoAdapter: VideoAdapter
     private var binding: FragmentPlayerBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -18,15 +31,30 @@ class PlayerFragment: Fragment(R.layout.fragment_player) {
         val fragmentPlayerBinding = FragmentPlayerBinding.bind(view)
         binding = fragmentPlayerBinding
 
-        // binding을 사용하지 않는 이유 : null 처리를 안해줘도 된다.
-        // MainActivity와 PlayerFragment를 이어줘서 같이 모션을 취하게 하는 코드
-        fragmentPlayerBinding.playerMotionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+
+        initMotionLayoutEvent(fragmentPlayerBinding)
+        initRecyclerView(fragmentPlayerBinding)
+
+        getVideoList()
+    }
+
+    // binding을 사용하지 않는 이유 : null 처리를 안해줘도 된다.
+    // MainActivity와 PlayerFragment를 이어줘서 같이 모션을 취하게 하는 코드
+    private fun initMotionLayoutEvent(fragmentPlayerBinding: FragmentPlayerBinding) {
+        fragmentPlayerBinding.playerMotionLayout.setTransitionListener(object :
+            MotionLayout.TransitionListener {
             override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
 
-            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
                 binding?.let {
                     (activity as MainActivity).also { mainActivity ->
-                        mainActivity.findViewById<MotionLayout>(R.id.mainMotionLayout).progress = abs(progress)
+                        mainActivity.findViewById<MotionLayout>(R.id.mainMotionLayout).progress =
+                            abs(progress)
                     }
                 }
             }
@@ -35,6 +63,50 @@ class PlayerFragment: Fragment(R.layout.fragment_player) {
 
             override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
         })
+    }
+
+    private fun initRecyclerView(fragmentPlayerBinding: FragmentPlayerBinding) {
+        videoAdapter = VideoAdapter(callback = { url, title ->
+            play(url, title)
+        })
+        fragmentPlayerBinding.fragmentRecyclerView.apply {
+            adapter = videoAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    private fun getVideoList() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://run.mocky.io/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(VideoService::class.java).also {
+            it.listVideos()
+                .enqueue(object : Callback<VideoDto> {
+                    override fun onResponse(call: Call<VideoDto>, response: Response<VideoDto>) {
+                        if (response.isSuccessful.not()) {
+                            Log.d("FFFFFF", "response fail")
+                            return
+                        }
+
+                        response.body()?.let { dto ->
+                            videoAdapter.submitList(dto.videos)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<VideoDto>, t: Throwable) {
+                        Toast.makeText(context, "Fail", Toast.LENGTH_SHORT).show()
+                    }
+                })
+        }
+    }
+
+    fun play(url: String, title: String) {
+        binding?.let {
+            it.playerMotionLayout.transitionToEnd()
+            it.bottomTitleTextView.text = title
+        }
     }
 
     override fun onDestroy() {

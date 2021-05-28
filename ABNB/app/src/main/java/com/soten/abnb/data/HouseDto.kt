4df05b7994b67ea9775b19dabe11969ad6b652a0
Choiyounho <maxosa78@gmail.com>
2021-05-29 01:33:53 +0900

    [docs] : (유튜브) add gradle glide

--- ABNB/app/src/main/java/com/soten/abnb/data/HouseDto.kt
+++
@@ -1,5 +0,0 @@
-package com.soten.abnb.data
-
-data class HouseDto(
-    val items: List<HouseModel>
-)
---
+++ ABNB/app/src/main/java/com/soten/abnb/dto/HouseDto.kt
@@ -0,0 +1,7 @@
+package com.soten.abnb.dto
+
+import com.soten.abnb.data.HouseModel
+
+data class HouseDto(
+    val items: List<HouseModel>
+)

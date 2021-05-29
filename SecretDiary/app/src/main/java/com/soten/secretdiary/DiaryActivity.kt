084a38e167fcd929a10eaf45fb2120f9d1953ebb
Choiyounho <maxosa78@gmail.com>
2021-05-29 18:50:23 +0900

    [refactor] : (비밀 다이어리) 코드 리팩토링
    반복되는 코드 상수화, 메서드 분리

--- SecretDiary/app/src/main/java/com/soten/secretdiary/DiaryActivity.kt
+++ SecretDiary/app/src/main/java/com/soten/secretdiary/DiaryActivity.kt
@@ -1,27 +1,25 @@
 package com.soten.secretdiary
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import androidx.core.widget.addTextChangedListener
 import com.soten.secretdiary.databinding.ActivityDiaryBinding
-import com.soten.secretdiary.databinding.ActivityMainBinding
 
 class DiaryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDiaryBinding
 
-    private val handler = Handler(Looper.getMainLooper()) // 메인스레드에 연결 된 핸들러러
+    private val handler = Handler(Looper.getMainLooper()) // 메인스레드에 연결 된 핸들러
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDiaryBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
         val detailPreferences = getSharedPreferences(GET_SHARED_PREFERENCES, Context.MODE_PRIVATE)
 
@@ -36,15 +34,15 @@ class DiaryActivity : AppCompatActivity() {
         }
 
         binding.diaryEditText.addTextChangedListener {
-
             Log.d("DiaryActivity", "Text :: $it")
             handler.removeCallbacks(runnable)
-            handler.postDelayed(runnable, 500)
+            handler.postDelayed(runnable, HANDLER_SAVE_DELAYED)
         }
     }
 
     companion object {
         private const val GET_SHARED_PREFERENCES = "diary"
         private const val PUT_SHARED_PREFERENCES = "detail"
+        private const val HANDLER_SAVE_DELAYED = 500L
     }
 }
--- SecretDiary/app/src/main/java/com/soten/secretdiary/MainActivity.kt
+++ SecretDiary/app/src/main/java/com/soten/secretdiary/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.soten.secretdiary
 
-import android.content.Context
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import com.soten.secretdiary.databinding.ActivityMainBinding
 
@@ -19,31 +18,49 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
         initNumberPicker()
+        initOpenButton()
+        initChangePasswordButton()
+    }
 
+    private fun initNumberPicker() {
+        binding.numberPicker1.minValue = NUMBER_PICKER_MIN_VALUE
+        binding.numberPicker1.maxValue = NUMBER_PICKER_MAX_VALUE
+
+        binding.numberPicker2.minValue = NUMBER_PICKER_MIN_VALUE
+        binding.numberPicker2.maxValue = NUMBER_PICKER_MAX_VALUE
+
+        binding.numberPicker3.minValue = NUMBER_PICKER_MIN_VALUE
+        binding.numberPicker3.maxValue = NUMBER_PICKER_MAX_VALUE
+    }
+
+    private fun initOpenButton() {
         binding.openButton.setOnClickListener {
             if (changePasswordMode) {
                 Toast.makeText(this, TOAST_CHANGE_PASSWORD_ING, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            val passwordPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+            val passwordPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
             val passwordFromUser =
                 "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
 
-            if (passwordPreferences.getString(SHARED_PREFERENCES_NAME, DEFAULT_PASSWORD).equals(passwordFromUser)) {
+            if (passwordPreferences.getString(SHARED_PREFERENCES_NAME, DEFAULT_PASSWORD)
+                    .equals(passwordFromUser)
+            ) {
                 startActivity(Intent(this, DiaryActivity::class.java))
             } else {
                 showErrorAlertDialog()
             }
         }
+    }
 
+    private fun initChangePasswordButton() {
         binding.changePasswordButton.setOnClickListener {
 
-            val passwordPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+            val passwordPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
             val passwordFromUser =
                 "${binding.numberPicker1.value}${binding.numberPicker2.value}${binding.numberPicker3.value}"
 
@@ -53,18 +70,21 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 changePasswordMode = false
-                binding.changePasswordButton.setBackgroundColor(Color.BLACK)
+                changePasswordState(Color.BLACK)
             } else {
                 if (passwordPreferences.getString(SHARED_PREFERENCES_NAME, DEFAULT_PASSWORD).equals(passwordFromUser)) {
                     changePasswordMode = true
                     Toast.makeText(this, TOAST_CHANGE_PASSWORD, Toast.LENGTH_SHORT).show()
-                    binding.changePasswordButton.setBackgroundColor(Color.RED)
-
+                    changePasswordState(Color.RED)
                 } else {
                     showErrorAlertDialog()
                 }
             }
         }
+    }
+
+    private fun changePasswordState(color: Int) {
+        binding.changePasswordButton.setBackgroundColor(color)
     }
 
     private fun showErrorAlertDialog() {
@@ -76,17 +96,6 @@ class MainActivity : AppCompatActivity() {
             .show()
     }
 
-    private fun initNumberPicker() {
-        binding.numberPicker1.minValue = 0
-        binding.numberPicker1.maxValue = 9
-
-        binding.numberPicker2.minValue = 0
-        binding.numberPicker2.maxValue = 9
-
-        binding.numberPicker3.minValue = 0
-        binding.numberPicker3.maxValue = 9
-    }
-
     companion object {
         private const val ALERT_DIALOG_TITLE = "실패!!"
         private const val ALERT_DIALOG_POSITIVE = "확인!!"
@@ -94,6 +103,9 @@ class MainActivity : AppCompatActivity() {
 
         private const val TOAST_CHANGE_PASSWORD = "변경할 패스워드를 입력해주세요"
         private const val TOAST_CHANGE_PASSWORD_ING = "비밀번호 변경 중입니다"
+
+        private const val NUMBER_PICKER_MIN_VALUE = 0
+        private const val NUMBER_PICKER_MAX_VALUE = 9
 
         private const val DEFAULT_PASSWORD = "000"
 

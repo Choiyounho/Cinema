4ab7f9db0c017e244209fc29993685c4a1443b54
Choiyounho <maxosa78@gmail.com>
2021-07-06 17:52:12 +0900

    [feat] : (위치 검색) 현재 위치 불러오기 기능 구현

--- LocationSearch/app/src/main/java/com/soten/locationsearch/MapActivity.kt
+++ LocationSearch/app/src/main/java/com/soten/locationsearch/MapActivity.kt
@@ -1,34 +1,172 @@
 package com.soten.locationsearch
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.soten.locationsearch.databinding.ActivityMapBinding
+import com.soten.locationsearch.model.LocationLatLngEntity
+import com.soten.locationsearch.model.SearchResultEntity
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMapBinding
+    private lateinit var map: GoogleMap
+    private var currentSelectMarker: Marker? = null
+
+    private lateinit var searchResult: SearchResultEntity
+    private lateinit var locationManager: LocationManager
+
+    private lateinit var myLocationListener: MyLocationListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupGoogleMap()
+        // 데이터가 존재하지 않다면, MainActivity 에서 가져온다.
+        if (::searchResult.isInitialized.not()) {
+            intent?.let {
+                searchResult =
+                    it.getParcelableExtra(SEARCH_RESULT_EXTRA_KEY) ?: throw Exception("데이터가 존재 x")
+                setupGoogleMap()
+            }
+        }
+
+        bindViews()
+    }
+
+    private fun bindViews() = with(binding) {
+        currentLocationButton.setOnClickListener {
+            getMyLocation()
+        }
+    }
+
+    private fun getMyLocation() {
+        if (::locationManager.isInitialized.not()) {
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
+
+        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if (isGpsEnabled) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    PERMISSION_REQUEST_CODE
+                )
+            } else {
+                setMyLocationListener()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setMyLocationListener() {
+        val minTime = 1500L
+        val minDistance = 100f
+
+        if (::myLocationListener.isInitialized.not()) {
+            myLocationListener = MyLocationListener()
+        }
+
+        with(locationManager) {
+            requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime,
+                minDistance,
+                myLocationListener
+            )
+
+            requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime,
+                minDistance,
+                myLocationListener
+            )
+        }
     }
 
     private fun setupGoogleMap() {
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(map: GoogleMap) {
+        this.map = map
+        currentSelectMarker = setUpMarker(searchResult)
 
+        currentSelectMarker?.showInfoWindow()
+    }
+
+    private fun setUpMarker(searchResultEntity: SearchResultEntity): Marker? {
+        val positionLatLng = LatLng(
+            searchResultEntity.locationLatLng.latitude.toDouble(),
+            searchResultEntity.locationLatLng.longitude.toDouble()
+        )
+
+        val markerOptions = MarkerOptions().apply {
+            position(positionLatLng)
+            title(searchResultEntity.name)
+            snippet(searchResultEntity.fullAddress)
+        }
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(positionLatLng, CAMERA_ZOOM_LEVEL))
+
+        return map.addMarker(markerOptions)
+    }
+
+    private fun onCurrentLocationChanged(locationLatLngEntity: LocationLatLngEntity) {
+        map.moveCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(
+                    locationLatLngEntity.latitude.toDouble(),
+                    locationLatLngEntity.longitude.toDouble()
+                ), CAMERA_ZOOM_LEVEL
+            )
+        )
     }
 
     companion object {
-        val SEARCH_RESULT_EXTRA_KEY = "SEARCH_RESULT_EXTRA_KEY"
+        const val SEARCH_RESULT_EXTRA_KEY = "SEARCH_RESULT_EXTRA_KEY"
+        const val CAMERA_ZOOM_LEVEL = 17f
+
+        const val PERMISSION_REQUEST_CODE = 101
+    }
+
+    inner class MyLocationListener : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            val locationLatLngEntity = LocationLatLngEntity(
+                location.latitude.toFloat(),
+                location.longitude.toFloat()
+            )
+
+            onCurrentLocationChanged(locationLatLngEntity)
+        }
     }
 }

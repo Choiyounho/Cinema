9e6ab84d0463b6c95a34b807dcefb67a9a4fe7e8
Choiyounho <maxosa78@gmail.com>
2021-07-07 14:19:11 +0900

    [feat] : (OTT) 모션 레이아웃 구성

--- OTT/app/src/main/java/com/soten/ott/MainActivity.kt
+++ OTT/app/src/main/java/com/soten/ott/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.soten.ott
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.soten.ott.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var isMotionAnimation: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.scrollView.viewTreeObserver.addOnScrollChangedListener {
+            if (binding.scrollView.scrollY > 150f.dpToPx(this).toInt()) {
+                if (isMotionAnimation.not()) {
+                    binding.gatheringDigitalThingsLayout.transitionToEnd()
+                }
+            } else {
+                if (isMotionAnimation.not()) {
+                    binding.gatheringDigitalThingsLayout.transitionToStart()
+                }
+            }
+        }
+
+        binding.gatheringDigitalThingsLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                isMotionAnimation = true
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                isMotionAnimation = false
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+        })
+    }
+
+    private fun Float.dpToPx(context: Context): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            this,
+            context.resources.displayMetrics
+        )
     }
 }

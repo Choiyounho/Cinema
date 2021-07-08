4546f83714a78e66645d6253d40b1b6cb1a7015b
Choiyounho <maxosa78@gmail.com>
2021-07-08 12:17:56 +0900

    [feat] : (깃헙) 깃헙 로그인  AccessToken 통신 기능 구현

--- GithubRepository/app/src/main/java/com/soten/githubrepository/MainActivity.kt
+++ GithubRepository/app/src/main/java/com/soten/githubrepository/MainActivity.kt
@@ -1,11 +1,105 @@
 package com.soten.githubrepository
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import com.soten.githubrepository.databinding.ActivityMainBinding
+import com.soten.githubrepository.utility.AuthTokenProvider
+import com.soten.githubrepository.utility.RetrofitUtil
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var job: Job
+
+    private val authTokenProvider by lazy { AuthTokenProvider(this) }
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        job = Job()
+
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        loginButton.setOnClickListener {
+            loginGithub()
+        }
+    }
+
+    private fun loginGithub() {
+        val loginUri = Uri.Builder().scheme("https").authority("github.com")
+            .appendPath("login")
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id", BuildConfig.GITHUB_CLIENT_ID)
+            .build()
+
+        CustomTabsIntent.Builder().build().also {
+            it.launchUrl(this, loginUri)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        intent?.data?.getQueryParameter("code")?.let {
+            launch(coroutineContext) {
+                showProgress()
+                val getAccessTokenJob = getAccessToken(it)
+                disMissProgress()
+            }
+        }
+    }
+
+    private suspend fun showProgress() = withContext(coroutineContext) {
+        with(binding) {
+            loginButton.isGone = true
+            progressBar.isGone = false
+            progressTextView.isGone = false
+        }
+    }
+
+    private suspend fun disMissProgress() = withContext(coroutineContext) {
+        with(binding) {
+            loginButton.isGone = false
+            progressBar.isGone = true
+            progressTextView.isGone = true
+        }
+    }
+
+    private suspend fun getAccessToken(code: String) = withContext(Dispatchers.IO) {
+        val response = RetrofitUtil.authApiService.getAccessToken(
+            clientId = BuildConfig.GITHUB_CLIENT_ID,
+            clientSecret = BuildConfig.GITHUB_CLIENT_SECRET,
+            code = code
+        )
+
+        if (response.isSuccessful) {
+            val accessToken = response.body()?.accessToken ?: "xx"
+            Log.e("accessToken", accessToken)
+            if (accessToken.isNotEmpty()) {
+                authTokenProvider.updateToken(accessToken)
+            } else {
+                Toast.makeText(this@MainActivity, "accessToken이 존재 하지 않습니다.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
     }
 }
---
+++ GithubRepository/app/src/main/java/com/soten/githubrepository/data/response/GithubAccessTokenResponse.kt
@@ -0,0 +1,7 @@
+package com.soten.githubrepository.data.response
+
+class GithubAccessTokenResponse(
+    val accessToken: String,
+    val scope: String,
+    val tokenType: String
+)

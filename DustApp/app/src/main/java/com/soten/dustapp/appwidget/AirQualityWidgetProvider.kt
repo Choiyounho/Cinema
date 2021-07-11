abb3f2c2578b21ef06820bd3b07af5f21e36c0d0
Choiyounho <maxosa78@gmail.com>
2021-07-12 01:55:46 +0900

    [feat] : (미세먼지) 위젯 프로바이더 구현

---
+++ DustApp/app/src/main/java/com/soten/dustapp/appwidget/AirQualityWidgetProvider.kt
@@ -0,0 +1,18 @@
+package com.soten.dustapp.appwidget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class AirQualityWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+    }
+
+}

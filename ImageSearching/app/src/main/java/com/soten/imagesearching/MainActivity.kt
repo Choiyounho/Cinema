c01d03b5ed865ecaf9d1755c8545e4a99c1f7883
Choiyounho <maxosa78@gmail.com>
2021-07-14 15:21:51 +0900

    [feat] : (이미지검색) 사진 다운로드 기능 구현

--- ImageSearching/app/src/main/java/com/soten/imagesearching/MainActivity.kt
+++ ImageSearching/app/src/main/java/com/soten/imagesearching/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.soten.imagesearching
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.google.android.material.snackbar.Snackbar
 import com.soten.imagesearching.data.Repository
+import com.soten.imagesearching.data.models.PhotoResponse
 import com.soten.imagesearching.databinding.ActivityMainBinding
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
@@ -28,7 +43,12 @@ class MainActivity : AppCompatActivity() {
 
         initViews()
         bindView()
-        fetchRandomPhotos()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            fetchRandomPhotos()
+        } else {
+            requestWriteStoragePermission()
+        }
     }
 
     private fun initViews() = with(binding) {
@@ -54,6 +74,10 @@ class MainActivity : AppCompatActivity() {
         refreshLayout.setOnRefreshListener {
             fetchRandomPhotos(binding.searchEditText.text.toString())
         }
+
+        (recyclerView.adapter as? PhotoAdapter)?.onClickPhoto = { photo ->
+            showDownloadPhotoConfirmationDialog(photo)
+        }
     }
 
     private fun fetchRandomPhotos(query: String? = null) = scope.launch {
@@ -75,8 +99,116 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun showDownloadPhotoConfirmationDialog(photo: PhotoResponse) {
+        AlertDialog.Builder(this)
+            .setMessage("이 사진을 저장 하시겠습니까?")
+            .setPositiveButton("저장") { dialog, _ ->
+                downloadPhoto(photo.urls?.full)
+                dialog.dismiss()
+            }
+            .setNegativeButton("취소") { dialog, _ ->
+
+                dialog.dismiss()
+            }.show()
+    }
+
+    private fun requestWriteStoragePermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            REQUEST_WRITE_PERMISSION
+        )
+    }
+
+    private fun downloadPhoto(photoUrl: String?) {
+        photoUrl ?: return
+
+        Glide.with(this)
+            .asBitmap()
+            .load(photoUrl)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(
+                object : CustomTarget<Bitmap>(SIZE_ORIGINAL, SIZE_ORIGINAL) {
+                    override fun onResourceReady( // 다운로드 끝난 상태
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                    ) {
+                        saveBitmapToMediaStore(resource)
+                    }
+
+                    override fun onLoadStarted(placeholder: Drawable?) {
+                        Snackbar.make(binding.root, "다운로드 중...", Snackbar.LENGTH_INDEFINITE).show()
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {}
+
+                    override fun onLoadFailed(errorDrawable: Drawable?) {
+                        super.onLoadFailed(errorDrawable)
+                        Snackbar.make(binding.root, "다운로드 실패", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+            )
+    }
+
+    private fun saveBitmapToMediaStore(bitmap: Bitmap) {
+        val fileName = "${System.currentTimeMillis()}.jpg"
+        val resolver = applicationContext.contentResolver
+        val imageCollectionUri =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                MediaStore.Images.Media.getContentUri(
+                    MediaStore.VOLUME_EXTERNAL_PRIMARY
+                )
+            } else {
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            }
+        val imageDetails = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                put(MediaStore.Images.Media.IS_PENDING, 1)
+            }
+        }
+
+        val imageUri = resolver.insert(imageCollectionUri, imageDetails)
+
+        imageUri ?: return
+
+        resolver.openOutputStream(imageUri).use { outputStream ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            imageDetails.clear()
+            imageDetails.put(MediaStore.Images.Media.IS_PENDING, 0)
+            resolver.update(imageUri, imageDetails, null, null)
+        }
+
+        Snackbar.make(binding.root, "다운로드 완료", Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val writeExternalStoragePermission =
+            requestCode == REQUEST_WRITE_PERMISSION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if (writeExternalStoragePermission) {
+            fetchRandomPhotos()
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         scope.cancel()
+    }
+
+    companion object {
+        private const val REQUEST_WRITE_PERMISSION = 500
     }
 }

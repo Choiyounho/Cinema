fd0efc46d670ef5fb7bc22c4ba3d88646034bebb
Choiyounho <maxosa78@gmail.com>
2021-07-15 15:20:41 +0900

    [docs] : (todo) 클린 아키텍쳐 빌드업

---
+++ TODO/app/src/main/java/com/soten/todo/TodoApplication.kt
@@ -0,0 +1,12 @@
+package com.soten.todo
+
+import android.app.Application
+
+class TodoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // TODO Koin Trigger
+
+    }
+}

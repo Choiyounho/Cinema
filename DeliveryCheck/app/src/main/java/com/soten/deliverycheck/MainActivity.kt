2b24350ce00295286f7db881347f993633aaca2d
Choiyounho <maxosa78@gmail.com>
2021-07-27 10:47:04 +0900

    [feat] : (택배조회) 네비게이션 셋팅

--- DeliveryCheck/app/src/main/java/com/soten/deliverycheck/MainActivity.kt
+++ DeliveryCheck/app/src/main/java/com/soten/deliverycheck/MainActivity.kt
@@ -2,10 +2,26 @@ package com.soten.deliverycheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.soten.deliverycheck.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        // navigation 에 등록한 label 과 toolbar 연결
+        val navigationController =
+            (supportFragmentManager.findFragmentById(R.id.mainNavigationHostContainer) as NavHostFragment).navController
+        binding.toolbar.setupWithNavController(navigationController)
     }
 }
---
+++ DeliveryCheck/app/src/main/java/com/soten/deliverycheck/TrackingItemsFragment.kt
@@ -0,0 +1,6 @@
+package com.soten.deliverycheck
+
+import org.koin.android.scope.ScopeFragment
+
+class TrackingItemsFragment : ScopeFragment() {
+}

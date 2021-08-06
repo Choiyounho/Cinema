e9a04aba33a3669431a091566ecb498bd3ddc21e
Choiyounho <maxosa78@gmail.com>
2021-08-06 17:52:57 +0900

    [feat] : (배달) koin setup

--- FoodDelivery/app/src/main/java/com/soten/fooddelivery/DeliveryApplication.kt
+++ FoodDelivery/app/src/main/java/com/soten/fooddelivery/DeliveryApplication.kt
@@ -2,12 +2,18 @@ package com.soten.fooddelivery
 
 import android.app.Application
 import android.content.Context
+import com.soten.fooddelivery.di.appModule
+import org.koin.core.context.startKoin
 
 class DeliveryApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         appContext = this
+
+        startKoin {
+            modules(appModule)
+        }
     }
 
     override fun onTerminate() {
--- FoodDelivery/app/src/main/java/com/soten/fooddelivery/di/AppModule.kt
+++ FoodDelivery/app/src/main/java/com/soten/fooddelivery/di/AppModule.kt
@@ -1,4 +1,28 @@
 package com.soten.fooddelivery.di
 
-class AppModule {
+import com.soten.fooddelivery.util.provider.ResourceProvider
+import com.soten.fooddelivery.util.provider.ResourceProviderImpl
+import kotlinx.coroutines.Dispatchers
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+
+val appModule = module {
+
+    single { provideGsonConvertFactory() }
+    single { buildOkHttpClient() }
+
+    single { provideRetrofit(get(), get()) }
+
+    single<ResourceProvider> { ResourceProviderImpl(androidApplication()) }
+
+    // Coroutine
+    single { Dispatchers.IO }
+    single { Dispatchers.Main }
+
 }
+
+/*
+* 레퍼지토리패턴을 사용 시
+* 1. 보일러플레이트 코드
+* 2.
+* */
